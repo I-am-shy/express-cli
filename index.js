691f50189f721ef8200b160dcd16839302a0d469
shyshi <shyshi@zego.im>
2025-04-02 14:44:29 +0800

    增加git模版

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 import answer from './input.js'
+import fs from 'node:fs'
+import path from 'node:path'
 
-console.log(answer)
+// console.log(answer) // 用户输入的结果
+// console.log(process.cwd()) // 当前工作目录
+// console.log(import.meta.dirname) // 程序所在的目录
+
+if(!answer){
+  console.error('输入错误')
+  process.exit()
+}
+
+// 根据是否启用git选择模板
+const templatePath = answer.isGit ? path.join(import.meta.dirname, 'template_git') : path.join(import.meta.dirname, 'template')
+const resultPath = path.join(process.cwd(), answer.project_name)
+
+// 如果目录存在，则退出
+if(fs.existsSync(resultPath)){
+  console.error('当前目录已存在')
+  process.exit(1)
+}
+
+// 创建项目
+fs.cp(templatePath, resultPath, {recursive: true},(err)=>{
+  if(err){
+    console.error(err)
+    process.exit(1)
+  }
+  console.log('项目创建成功\n')
+  console.log(`cd ${answer.project_name}`)
+  console.log('npm install')
+  console.log('npm run start')
+  process.exit(0)
+})
+
--- input.js
+++ input.js
@@ -6,11 +6,11 @@ const prompt = {
     message: '请输入项目名称',
     default: 'my-project'
   },
-  project_type: {
-    message: '请选择语言类型',
-    choices: ['Javascript','Typescript' ],
-    default: 'Javascript'
-  },
+  // project_type: {
+  //   message: '请选择语言类型',
+  //   choices: ['Javascript','Typescript' ],
+  //   default: 'Javascript'
+  // },
   isGit: {
     message: '是否启用git进行版本管理',
     default: true
@@ -18,17 +18,20 @@ const prompt = {
 }
 
 try {
-  const inputAnswer = await input(prompt.project_name);
+  if(prompt.project_name){
+    const inputAnswer = await input(prompt.project_name);
+    Object.assign(answer, {project_name:inputAnswer})
+  }
 
-  Object.assign(answer, {project_name:inputAnswer})
+  if(prompt.project_type){
+    const selectAnswer = await select(prompt.project_type);
+    Object.assign(answer, {project_type:selectAnswer})
+  }
 
-  const selectAnswer = await select(prompt.project_type);
-
-  Object.assign(answer, {project_type:selectAnswer})
-
-  const confirmAnswer = await confirm(prompt.isGit);
-
-  Object.assign(answer, {isGit:confirmAnswer})
+  if(prompt.isGit){
+    const confirmAnswer = await confirm(prompt.isGit);
+    Object.assign(answer, {isGit:confirmAnswer})
+  }
 
 } catch (error) { // 捕获异常,防止退出时出现promise失败的异常
   console.warn("退出")
